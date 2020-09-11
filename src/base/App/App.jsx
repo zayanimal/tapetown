add86bb3a8e7d37638ea8f3be292b027339234ed
Vladimir Zaychikov <zaychikov.vl@gmail.com>
2020-09-11 15:46:13 +0300

    десктопная версия для обложек, анализ цвета фонового изображения

--- src/base/App/App.jsx
+++ src/base/App/App.jsx
@@ -1,4 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import FastAverageColor from 'fast-average-color';
 import { Layout } from '@base/Layout';
+import background from '@images/bckg_desktop.png';
 
-export const App = () => <Layout />;
+const analyzeColor = new FastAverageColor();
+
+export const App = () => {
+    useEffect(() => {
+        /** анализ картинки бэкграунда
+         * на преобладающий цвет
+         */
+        analyzeColor.getColorAsync(background, { algorithm: 'dominant' })
+            .then((color) => document
+                .querySelector('body')
+                .style
+                .setProperty('background-color', color.hex)
+            );
+
+        /** очистка памяти */
+        analyzeColor.destroy();
+    });
+
+    return <Layout />;
+};
--- src/base/Layout/Layout.jsx
+++ src/base/Layout/Layout.jsx
@@ -11,86 +11,93 @@ import './Layout.scss';
 
 const cn = bem('Layout');
 
-export const Layout = () => (
-    <div className={cn()}>
-        <div className={cn('background')}>
-            <div className="row">
-                <div className={cn('title')}>
-                    <div className={cn('title-text')}>
-                        Tapetown
-                    </div>
-                    <div className={cn('title-sub')}>
-                        <span>Radio</span>
-                        <span className={cn('title-sub_tv')}>Tv</span>
+export const Layout = () => {
+    return (
+        <div className={cn()}>
+            <div className={cn('background')}>
+                <div className="row">
+                    <div className={cn('title')}>
+                        <div className={cn('title-text')}>
+                            Tapetown
+                        </div>
+                        <div className={cn('title-sub')}>
+                            <span>Radio</span>
+                            <span className={cn('title-sub_tv')}>Tv</span>
+                        </div>
                     </div>
                 </div>
+                <div className={cn('container-1')}>
+                    {/* Воспроизведение */}
+                    <section className={cn('now')}>
+                        <h4>Now:</h4>
+                        <Player />
+                    </section>
+
+                    {/* Предстоящие выпуски */}
+                    <section className={cn('upcoming')}>
+                        <h4>Upcoming:</h4>
+
+                        <h1>Aug 12</h1>
+
+                        <div className={cn('upcoming-header')}>
+                            <span>Plotno Slozheno</span>
+                        </div>
+                        <div className={cn('upcoming-description')}>
+                            <span>w/ Endemic</span>
+                        </div>
+                    </section>
+
+                    {/* Брэнд */}
+                    <section className={cn('brand')}>
+                        <span>tapetown.live</span>
+                        <img src={tapetown} alt="tapetown" />
+                    </section>
+
+                    {/* Ссылки */}
+                    <section className={cn('links')}>
+                        <h4>Links:</h4>
+
+                        <a href="/">Facebook</a>
+                        <a href="/">Instagram</a>
+                        <a href="/">Soundcloud</a>
+                        <a href="/">Telegram</a>
+                        <a href="/">Donate</a>
+                    </section>
+
+                    {/* О нас */}
+                    <section className={cn('about')}>
+                        <h4>About:</h4>
+
+                        <span>
+                            is an independent, DIY internet radio station broadcasting 24/7
+                            from small apartment in Kaliningrad City, occasionally streaming live mix
+                            sessions, hosted by two aspiring music enthusiasts.
+                        </span>
+                    </section>
+                </div>
             </div>
-            <div className={cn('container-1')}>
-                {/* Воспроизведение */}
-                <section className={cn('now')}>
-                    <h4>Now:</h4>
-                    <Player />
-                </section>
 
-                {/* Предстоящие выпуски */}
-                <section className={cn('upcoming')}>
-                    <h4>Upcoming:</h4>
+            {/* Прошедшие мероприятия */}
+            <section className={classes('row', cn('past'))}>
+                <h4 className={cn('past-title')}>Past:</h4>
+                <div className={cn('past-list')}>
+                    <img className={cn('past-img')} src={event1} alt="event1"/>
+                    <img className={cn('past-img')} src={event2} alt="event2"/>
+                    <img className={cn('past-img')} src={event3} alt="event3"/>
+                    <img className={cn('past-img')} src={event4} alt="event4"/>
+                    <img className={cn('past-img')} src={event1} alt="event1"/>
+                    <img className={cn('past-img')} src={event2} alt="event2"/>
+                    <img className={cn('past-img')} src={event3} alt="event3"/>
+                    <img className={cn('past-img')} src={event4} alt="event4"/>
+                </div>
+            </section>
 
-                    <h1>Aug 12</h1>
-
-                    <div className={cn('upcoming-header')}>
-                        <span>Plotno Slozheno</span>
-                    </div>
-                    <div className={cn('upcoming-description')}>
-                        <span>w/ Endemic</span>
-                    </div>
-                </section>
-
-                {/* Брэнд */}
-                <section className={cn('brand')}>
-                    <span>tapetown.live</span>
-                    <img src={tapetown} alt="tapetown" />
-                </section>
-
-                {/* Ссылки */}
-                <section className={cn('links')}>
-                    <h4>Links:</h4>
-
-                    <a href="/">Facebook</a>
-                    <a href="/">Instagram</a>
-                    <a href="/">Soundcloud</a>
-                    <a href="/">Telegram</a>
-                    <a href="/">Donate</a>
-                </section>
-
-                {/* О нас */}
-                <section className={cn('about')}>
-                    <h4>About:</h4>
-
-                    <span>
-                        is an independent, DIY internet radio station broadcasting 24/7
-                        from small apartment in Kaliningrad City, occasionally streaming live mix
-                        sessions, hosted by two aspiring music enthusiasts.
-                    </span>
-                </section>
-            </div>
+            {/* Футер */}
+            <section className={cn('footer')}>
+                {/* Горизонтальная линия */}
+                <div className={cn('line')} />
+                <img className={cn('footer-img')} src={footer} alt="footer"/>
+            </section>
         </div>
-
-        {/* Прошедшие мероприятия */}
-        <section className={classes('row', cn('past'))}>
-            <h4>Past:</h4>
-
-            <img className={cn('past-img')} src={event1} alt="event1"/>
-            <img className={cn('past-img')} src={event2} alt="event2"/>
-            <img className={cn('past-img')} src={event3} alt="event3"/>
-            <img className={cn('past-img')} src={event4} alt="event4"/>
-        </section>
-
-        {/* Футер */}
-        <section className={cn('footer')}>
-            {/* Горизонтальная линия */}
-            <div className={cn('line')} />
-            <img className={cn('footer-img')} src={footer} alt="footer"/>
-        </section>
-    </div>
-);
+    );
+};
