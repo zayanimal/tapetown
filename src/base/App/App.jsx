dde5c94c4a71a152cbe713a5a062f1851a617246
Vladimir Zaychikov <zaychikov.vl@gmail.com>
2020-09-17 11:43:17 +0300

    svg бэкграунд

--- src/base/App/App.jsx
+++ src/base/App/App.jsx
@@ -1,13 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import FastAverageColor from 'fast-average-color';
 import FontFaceObserver from 'fontfaceobserver';
 import { Layout } from '@base/Layout';
-import background from '@images/bckg_desktop.png';
 
 export const App = () => {
-    /** анализ цвета */
-    const analyzeColor = new FastAverageColor();
-
     /** наблюдение за загрукой веб шрифтов */
     const helvetica = new FontFaceObserver('Helvetica Neue');
     const fjalla = new FontFaceObserver('Fjalla One');
@@ -15,23 +10,10 @@ export const App = () => {
     const [fontFail, setFontFail] = useState('');
 
     useEffect(() => {
-        /** анализ картинки бэкграунда
-         * на преобладающий цвет
-         */
-        analyzeColor.getColorAsync(background, { algorithm: 'dominant' })
-            .then((color) => document
-                .querySelector('body')
-                .style
-                .setProperty('background-color', color.hex)
-            );
-
-        /** очистка памяти */
-        analyzeColor.destroy();
-
         Promise
             .all([helvetica.load(null, 2000), fjalla.load(null, 2000)])
             .catch(() => { setFontFail('Layout__title_fail'); });
-    }, [analyzeColor, helvetica, fjalla, setFontFail]);
+    }, [helvetica, fjalla, setFontFail]);
 
     return <Layout fontFailCls={fontFail} />;
 };
